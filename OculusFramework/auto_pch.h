bb2aaf9ae5b7825974ae9fcd5c15e50ceddf4311
Matt Newport <matt@mattnewport.com>
2015-05-15 11:11:41 -0700

    Bit the bullet and used Microsoft's automatic pch tool to add precompiled headers to the project.

---
+++ OculusFramework/auto_pch.h
@@ -0,0 +1,27 @@
+/* AUTOMATICALLY GENERATED CODE */
+
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <regex>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
+
+#include <d3d11_1.h>
+#include <immintrin.h>
+#include <nmmintrin.h>
+#include <windows.h>
+#include <wmmintrin.h>
+#include <xinput.h>
