18245bc4953bbd92d79b727464e76701b73a67f3
Matt Newport <matt@mattnewport.com>
2015-05-20 14:34:37 -0700

    Moved models to use common lighting code with sphere and terrain.

--- OculusFramework/simpleps.hlsl
+++ OculusFramework/simpleps.hlsl
@@ -1,14 +1,27 @@
+#include "lighting.hlsli"
+
 Texture2D Texture : register(t0);
 SamplerState Linear : register(s0);
 
 float4 main(in float4 Position : SV_Position, in float4 Color : COLOR0, 
-            in float2 TexCoord : TEXCOORD0, in float3 worldPos : TEXCOORD1) : SV_Target
+            in float2 TexCoord : TEXCOORD0, in float3 worldPos : TEXCOORD1, in float3 viewDir : TEXCOORD2) : SV_Target
 {
     float3 tan = ddx(worldPos);
     float3 bin = ddy(worldPos);
     float3 n = normalize(cross(bin, tan));
-    float3 l = float3(0, 3.7, 0) - worldPos;
-    float r = length(l);
-    float d = dot(n, l / r);
-    return Color * (0.5 + 10 * d / r) * Texture.Sample(Linear, TexCoord);
+    float3 l = lightPos - worldPos;
+    float lmag = length(l);
+
+    PhongMaterialParams mat;
+    float3 color = Color.xyz * Texture.Sample(Linear, TexCoord).xyz;
+    mat.ka = color;
+    mat.kd = color;
+    mat.ks = color;
+    mat.alpha = 10.0f;
+
+    PhongLightParams light;
+    light.ia = 0.2f;
+    light.i = 1.0f / lmag*lmag;
+
+    return float4(phong(mat, light, n, l / lmag, normalize(viewDir)), 1.0f);
 };
--- OculusFramework/simplevs.hlsl
+++ OculusFramework/simplevs.hlsl
@@ -2,11 +2,12 @@
 
 void main(in float4 Position : POSITION, in float4 Color : COLOR0, in float2 TexCoord : TEXCOORD0,
           out float4 oPosition : SV_Position, out float4 oColor : COLOR0, out float2 oTexCoord : TEXCOORD0,
-          out float3 worldPos : TEXCOORD1)
+          out float3 worldPos : TEXCOORD1, out float3 viewDir : TEXCOORD2)
 {
     float4 wp = mul(object.world, Position);
     oPosition = mul(camera.proj, mul(camera.view, wp));
     oTexCoord = TexCoord;
     oColor = Color;
     worldPos = float3(wp.xyz);
+    viewDir = normalize(camera.eye - worldPos);
 }
