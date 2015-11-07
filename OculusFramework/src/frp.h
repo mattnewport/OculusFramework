a35e791b092a8eacb3caa387992f135af02f8914
Matt Newport <matt@mattnewport.com>
2015-11-07 14:18:41 -0800

    Bite the bullet and make Behaviour state shared via a shared_ptr. This simplifies things with regards to lifetime / identity of Behaviour state at the cost of a dynamic allocation.

--- OculusFramework/src/frp.h
+++ OculusFramework/src/frp.h
@@ -2,6 +2,7 @@
 
 #include <functional>
 #include <limits>
+#include <memory>
 #include <utility>
 
 namespace frp {
@@ -12,20 +13,25 @@ template <typename T>
 class Behaviour {
 public:
     Behaviour() = default;
-    Behaviour(std::function<T(TimeS)> f_) : f(f_) {}
-    T operator()(TimeS t) {
-        if (!f) return T{};
-        if (t != lastTime) {
-            lastValue = f(t);
-            lastTime = t;
-        }
-        return lastValue;
-    }
+    Behaviour(std::function<T(TimeS)> f) : state{std::make_shared<State>(f)} {}
+    T operator()(TimeS t) { return (*state)(t); }
 
 private:
-    TimeS lastTime = std::numeric_limits<TimeS>::min();
-    T lastValue;
-    std::function<T(TimeS)> f;
+    struct State {
+        State(std::function<T(TimeS)> f_) : f{f_} {}
+        T operator()(TimeS t) {
+            if (!f) return T{};
+            if (t != lastTime) {
+                lastValue = f(t);
+                lastTime = t;
+            }
+            return lastValue;
+        }
+        std::function<T(TimeS)> f;
+        TimeS lastTime = std::numeric_limits<TimeS>::min();
+        T lastValue = {};
+    };
+    std::shared_ptr<State> state;
 };
 
 template <typename F>
@@ -39,56 +45,33 @@ auto always(T x) {
 }
 
 template <typename T, typename F>
-auto map(Behaviour<T>& b, F f) {
-    return makeBehaviour([&b, f](TimeS t) mutable { return f(b(t)); });
-}
-
-template <typename T, typename F>
-auto map(Behaviour<T>&& b, F f) {
-    return makeBehaviour([b = std::move(b), f](TimeS t) mutable { return f(b(t)); });
+auto map(Behaviour<T> b, F f) {
+    return makeBehaviour([b, f](TimeS t) mutable { return f(b(t)); });
 }
 
 template <typename T, typename U, typename F>
-auto map(Behaviour<T>& b0, Behaviour<U>& b1, F f) {
-    return makeBehaviour([&b0, &b1, f](TimeS t) mutable { return f(b0(t), b1(t)); });
-}
-
-template <typename T, typename U, typename F>
-auto map(Behaviour<T>&& b0, Behaviour<U>& b1, F f) {
-    return makeBehaviour(
-        [b0 = std::move(b0), &b1, f](TimeS t) mutable { return f(b0(t), b1(t)); });
-}
-
-template <typename T, typename U, typename F>
-auto map(Behaviour<T>& b0, Behaviour<U>&& b1, F f) {
-    return makeBehaviour(
-        [&b0, b1 = std::move(b1), f](TimeS t) mutable { return f(b0(t), b1(t)); });
-}
-
-template <typename T, typename U, typename F>
-auto map(Behaviour<T>&& b0, Behaviour<U>&& b1, F f) {
-    return makeBehaviour(
-        [b0 = std::move(b0), b1 = std::move(b1), f](TimeS t) mutable { return f(b0(t), b1(t)); });
+auto map(Behaviour<T> b0, Behaviour<U> b1, F f) {
+    return makeBehaviour([b0, b1, f](TimeS t) mutable { return f(b0(t), b1(t)); });
 }
 
 template <typename T>
-auto choice(Behaviour<bool>& b, T whenTrue, T whenFalse) {
+auto choice(Behaviour<bool> b, T whenTrue, T whenFalse) {
     return map(b, [whenTrue, whenFalse](bool x) { return x ? whenTrue : whenFalse; });
 }
 
 template <typename T, typename U>
-auto operator*(Behaviour<T>& x, Behaviour<U>& y) {
-    return makeBehaviour([&x, &y](TimeS t) mutable { return x(t) * y(t); });
+auto operator*(Behaviour<T> x, Behaviour<U> y) {
+    return makeBehaviour([x, y](TimeS t) mutable { return x(t) * y(t); });
 }
 
 template <typename T, typename U>
-auto operator+(Behaviour<T>& x, Behaviour<U>& y) {
-    return makeBehaviour([&x, &y](TimeS t) mutable { return x(t) + y(t); });
+auto operator+(Behaviour<T> x, Behaviour<U> y) {
+    return makeBehaviour([x, y](TimeS t) mutable { return x(t) + y(t); });
 }
 
 template <typename T, typename U>
-auto operator-(Behaviour<T>& x, Behaviour<U>& y) {
-    return makeBehaviour([&x, &y](TimeS t) mutable { return x(t) - y(t); });
+auto operator-(Behaviour<T> x, Behaviour<U> y) {
+    return makeBehaviour([x, y](TimeS t) mutable { return x(t) - y(t); });
 }
 
 inline auto makeTimeDeltaBehaviour(TimeS startTime) {
@@ -100,7 +83,7 @@ inline auto makeTimeDeltaBehaviour(TimeS startTime) {
 }
 
 template <typename T>
-auto eulerIntegrate(T init, Behaviour<T>& rate, TimeS start) {
+auto eulerIntegrate(T init, Behaviour<T> rate, TimeS start) {
     return map(map(rate, makeTimeDeltaBehaviour(start), std::multiplies<>{}),
                [value = init](auto delta) mutable { return value += delta; });
 }
